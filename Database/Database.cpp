bf1ff8e22488686995340af40f51c0c0f4041c2a
Constantinos Bolosis <cblsss2011@hotmail.com>
2022-02-18 18:02:27 +0200

    Using thread safe SQLite

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -4,29 +4,19 @@
 #include <cstdlib>
 
 /* The filename of the database */
-#define __DB_FILENAME "database.db"
+#define DB_FILENAME "database.db"
 
 #ifdef _WIN32
-	#define UNICODE
 	#define WIN32_LEAN_AND_MEAN
 	#include <Windows.h>
 	#include <Shlwapi.h>
-	#define DB_FILENAME TEXT(__DB_FILENAME)
-	#define DB_FILENAME_SIZE (sizeof(DB_FILENAME) / sizeof(WCHAR))
-	#define SQLITE3_OPEN(filename, ppDB) sqlite3_open16(filename, ppDB)
 #else
 	#include <climits>
 	#include <cstring>
-	#define DB_FILENAME __DB_FILENAME
-	#define DB_FILENAME_SIZE (sizeof(DB_FILENAME))
-	#define SQLITE3_OPEN(filename, ppDB) sqlite3_open(filename, ppDB)
-	#define TCHAR char
 	#ifdef __APPLE__
 		#include <mach-o/dyld.h>
-		typedef uint32_t path_len_t;
 	#else
 		#include <unistd.h>
-		typedef size_t path_len_t;
 	#endif
 #endif
 
@@ -34,53 +24,60 @@ cDatabase cDatabase::ms_instance;
 
 cDatabase::cDatabase() {
 	/* Get the path of the database file */
-	TCHAR* db_filename = nullptr;
+	char* db_filename = nullptr;
 #ifdef _WIN32
 	DWORD dwSize = MAX_PATH;
 	for (LPWSTR p; (p = (LPWSTR)realloc(db_filename, dwSize * sizeof(WCHAR))); dwSize <<= 1) {
-		db_filename = p;
-		DWORD dwLen = GetModuleFileNameW(NULL, db_filename, dwSize);
+		db_filename = (char*)p;
+		DWORD dwLen = GetModuleFileNameW(NULL, p, dwSize);
 		if (dwLen == 0) break;
-		if (dwLen + DB_FILENAME_SIZE < dwSize) {
-			lstrcpyW(PathFindFileNameW(db_filename), DB_FILENAME);
-			goto LABEL;
+		if (dwLen < dwSize) {
+			int utf8size = WideCharToMultiByte(CP_UTF8, 0, p, -1, NULL, 0, NULL, NULL);
+			if (utf8size) {
+				if (char* q = (char*)malloc(utf8size + sizeof(DB_FILENAME))) {
+					free(db_filename);
+					db_filename = q;
+					WideCharToMultiByte(CP_UTF8, 0, p, -1, db_filename, utf8size, NULL, NULL);
+					strcpy(PathFindFileNameA(db_filename), DB_FILENAME);
+					goto LABEL_RESOLVED_DBNAME;
+				}
+			}
 		}
 	}
-#else
-	path_len_t size = PATH_MAX;
-#ifdef __APPLE__
+#elif defined(__APPLE__)
+	uint32_t size = PATH_MAX;
 	for (char* p; (p = (char*)realloc(db_filename, size)); db_filename = p) {
 		if (0 == _NSGetExecutablePath(p, &size)) {
 			(db_filename = realpath(p, nullptr)) ? free(p) : (void)(db_filename = p);
 			if ((p = strrchr(db_filename, '/'))) {
 				if (ptrdiff_t o = (p - db_filename) + 1; (p = (char*) realloc(db_filename, o + sizeof(DB_FILENAME)))) {
 					strcpy((db_filename = p) + o, DB_FILENAME);
-					goto LABEL;
+					goto LABEL_RESOLVED_DBNAME;
 				}
 			}
 		}
 	}
 #else
 	/* TODO add more link options */
+	size_t size = PATH_MAX;
 	for (char* p; (p = (char*)realloc(db_filename, size)); size <<= 1) {
 		ssize_t len = readlink("/proc/self/exe", db_filename = p, size);
 		if (len < 0) break;
 		if (len + sizeof(DB_FILENAME) < size) {
 			if (!(p = strrchr(db_filename, '/'))) break;
 			strcpy(p, DB_FILENAME);
-			goto LABEL;
+			goto LABEL_RESOLVED_DBNAME;
 		}
 	}
-#endif
 #endif
 	free(db_filename);
 	cUtils::PrintErr("Fatal initialisation error.");
 	exit(EXIT_FAILURE);
 
-LABEL:
+LABEL_RESOLVED_DBNAME:
 	/* Open a database handle */
 	sqlite3* db;
-	int rc = sqlite3_open_v2(db_filename, &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_FULLMUTEX, nullptr); //SQLITE3_OPEN(db_filename, &db);
+	int rc = sqlite3_open_v2(db_filename, &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_FULLMUTEX, nullptr);
 	free(db_filename);
 	/* If database handle is open, create leaderboard table */
 	if (rc == SQLITE_OK) {
