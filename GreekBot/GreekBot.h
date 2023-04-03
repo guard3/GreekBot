4d631be22e203e234798cac365a9c73a7f93d4d7
guard3 <cblsss2011@hotmail.com>
2023-04-04 01:13:12 +0300

    smth totally silly

--- GreekBot/GreekBot.h
+++ GreekBot/GreekBot.h
@@ -125,6 +125,6 @@ private:
 	cTask<> OnMessageCreate(const cMessage& msg) override;
 
 public:
-	explicit cGreekBot(const char* token) : cBot(token, INTENT_GUILD_INTEGRATIONS | INTENT_GUILD_MESSAGES | INTENT_GUILDS) {}
+	explicit cGreekBot(const char* token) : cBot(token, INTENT_GUILD_INTEGRATIONS | INTENT_GUILD_MESSAGES | INTENT_GUILDS | INTENT_DIRECT_MESSAGES) {}
 };
 #endif /* _GREEKBOT_GREEKBOT_H_ */
