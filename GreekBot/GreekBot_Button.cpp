924d621f6a3d23b349f3eb619c1503b28bb4624d
guard3 <cblsss2011@hotmail.com>
2023-08-02 16:35:19 +0300

    Change the response to leaderboard help button

--- GreekBot/GreekBot_Button.cpp
+++ GreekBot/GreekBot_Button.cpp
@@ -2,20 +2,6 @@
 
 cTask<>
 cGreekBot::OnInteraction_button(const cInteraction& i) {
-	try {
-		auto embeds = i.GetMessage()->MoveEmbeds();
-		cEmbed e {
-			kw::color=embeds.back().GetColor(),
-			kw::title="How it works",
-			kw::description="Every minute that you're messaging, you randomly gain between 15 and 25 **XP**."
-		};
-		if (embeds.size() == 10)
-			embeds[9] = std::move(e);
-		else
-			embeds.push_back(std::move(e));
-		co_await RespondToInteraction(i, kw::components=nil, kw::embeds=std::move(embeds));
-	}
-	catch (const std::exception& e) {
-		cUtils::PrintErr("OnInteraction_button: %s", e.what());
-	}
+	co_await RespondToInteraction(i);
+	co_await SendInteractionFollowupMessage(i, kw::flags=MESSAGE_FLAG_EPHEMERAL, kw::content="Every minute that you're messaging, you randomly gain between 15 and 25 **XP**.");
 }
