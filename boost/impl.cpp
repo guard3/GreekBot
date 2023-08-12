a3ceb4412e12ecd4cc54cec1b65578d8bf719efe
guard3 <cblsss2011@hotmail.com>
2023-08-12 17:23:40 +0300

    Make Boost/Beast compile separately

--- boost/impl.cpp
+++ boost/impl.cpp
@@ -1,3 +1,8 @@
-/* Compile Boost/ASIO separately */
+/* Compile Boost/ASIO and Boost/Beast separately */
+#ifdef BOOST_ASIO_SEPARATE_COMPILATION
 #include <boost/asio/impl/src.hpp>
 #include <boost/asio/ssl/impl/src.hpp>
+#endif
+#ifdef BOOST_BEAST_SEPARATE_COMPILATION
+#include <boost/beast/src.hpp>
+#endif
