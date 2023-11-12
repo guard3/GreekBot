b35f71080475abe2b3aec972c4e4499dee31cf80
guard3 <cblsss2011@hotmail.com>
2023-11-12 22:44:49 +0200

    Cleanup

--- Discord/Interaction/InteractionBase.cpp
+++ Discord/Interaction/InteractionBase.cpp
@@ -1,6 +1,6 @@
 #include "Interaction.h"
 #include "json.h"
-
+/* ================================================================================================================== */
 eInteractionType
 tag_invoke(json::value_to_tag<eInteractionType>, const json::value& v) {
 	return static_cast<eInteractionType>(v.to_number<int>());
@@ -13,25 +13,22 @@ eAppCmdOptionType
 tag_invoke(json::value_to_tag<eAppCmdOptionType>, const json::value& v) {
 	return static_cast<eAppCmdOptionType>(v.to_number<int>());
 }
-
-cAppCmdOption::user_data::user_data(const json::value& v, const json::value* p) : user(v) {
-	if (p)
-		member.emplace(*p);
+cAppCmdOption
+tag_invoke(json::value_to_tag<cAppCmdOption>, const json::value& v, cPtr<const json::value> r) {
+	return cAppCmdOption(v, r);
 }
-
+/* ========== A simple struct that holds a pair of user and an optional partial member ============================== */
+cAppCmdOption::user_data::user_data(const json::value& v, const json::value* p) : user(v) { if (p) member.emplace(*p); }
+/* ================================================================================================================== */
 cAppCmdOption::cAppCmdOption(const json::value& v, cPtr<const json::value> r):
 	m_name(json::value_to<std::string>(v.at("name"))),
 	m_type(json::value_to<eAppCmdOptionType>(v.at("type"))) {
 	switch (m_type) {
 		case APP_CMD_OPT_SUB_COMMAND:
-		case APP_CMD_OPT_SUB_COMMAND_GROUP: {
-			auto& o = m_value.emplace<options_t>();
-			auto& a = v.at("options").as_array();
-			o.reserve(a.size());
-			for (auto& e : a)
-				o.emplace_back(e, r);
+		case APP_CMD_OPT_SUB_COMMAND_GROUP:
+			typedef std::vector<cAppCmdOption> options_t;
+			m_value.emplace<options_t>(json::value_to<options_t>(v.at("options"), r));
 			break;
-		}
 		case APP_CMD_OPT_STRING:
 			m_value.emplace<std::string>(json::value_to<std::string>(v.at("value")));
 			break;
@@ -43,8 +40,9 @@ cAppCmdOption::cAppCmdOption(const json::value& v, cPtr<const json::value> r):
 			break;
 		case APP_CMD_OPT_USER: {
 			json::string_view s = v.at("value").as_string();
-			const json::value* a = r->as_object().if_contains("members");
-			m_value.emplace<user_data>(r->at("users").at(s), a ? a->as_object().if_contains(s) : nullptr);
+			const json::value* pValue = r->as_object().if_contains("members");
+			if (pValue) pValue = pValue->as_object().if_contains(s);
+			m_value.emplace<user_data>(r->at("users").at(s), pValue);
 			break;
 		}
 		case APP_CMD_OPT_CHANNEL:
@@ -52,14 +50,11 @@ cAppCmdOption::cAppCmdOption(const json::value& v, cPtr<const json::value> r):
 		case APP_CMD_OPT_MENTIONABLE:
 			m_value.emplace<cSnowflake>(json::value_to<std::string_view>(v.at("value")));
 			break;
-		case APP_CMD_OPT_NUMBER:
-			m_value.emplace<double>(v.at("value").as_double());
-			break;
 		default:
+			m_value.emplace<double>(v.at("value").as_double());
 			break;
 	}
 }
-
 cAppCmdOption::cAppCmdOption(eAppCmdType type, std::string_view id, const json::object& resolved) {
 	if (type == APP_CMD_USER) {
 		m_type = APP_CMD_OPT_USER;
@@ -67,7 +62,7 @@ cAppCmdOption::cAppCmdOption(eAppCmdType type, std::string_view id, const json::
 		m_value.emplace<user_data>(resolved.at("users").at(id), a ? a->as_object().if_contains(id) : nullptr);
 	}
 }
-
+/* ========== Option getters for subcommands ======================================================================== */
 std::span<const cAppCmdOption>
 cAppCmdOption::GetOptions() const noexcept {
 	if (auto p = std::get_if<std::vector<cAppCmdOption>>(&m_value))
@@ -86,13 +81,112 @@ cAppCmdOption::MoveOptions() noexcept {
 		return std::move(*p);
 	return {};
 }
-
-cInteraction::guild_data::guild_data(const json::value& s, const json::value& v): guild_id(json::value_to<cSnowflake>(s)), member(v) {}
-
+/* ================================================================================================================== */
+xAppCmdOptionTypeError::xAppCmdOptionTypeError(eAppCmdOptionType type) : std::invalid_argument([](eAppCmdOptionType type) {
+	const char* name;
+	switch (type) {
+		case APP_CMD_OPT_SUB_COMMAND:
+			name = "APP_CMD_OPT_SUB_COMMAND";
+			break;
+		case APP_CMD_OPT_SUB_COMMAND_GROUP:
+			name = "APP_CMD_OPT_SUB_COMMAND_GROUP";
+			break;
+		case APP_CMD_OPT_STRING:
+			name = "APP_CMD_OPT_STRING";
+			break;
+		case APP_CMD_OPT_INTEGER:
+			name = "APP_CMD_OPT_INTEGER";
+			break;
+		case APP_CMD_OPT_BOOLEAN:
+			name = "APP_CMD_OPT_BOOLEAN";
+			break;
+		case APP_CMD_OPT_USER:
+			name = "APP_CMD_OPT_USER";
+			break;
+		case APP_CMD_OPT_CHANNEL:
+			name = "APP_CMD_OPT_CHANNEL";
+			break;
+		case APP_CMD_OPT_ROLE:
+			name = "APP_CMD_OPT_ROLE";
+			break;
+		case APP_CMD_OPT_MENTIONABLE:
+			name = "APP_CMD_OPT_MENTIONABLE";
+			break;
+		default:
+			name = "APP_CMD_OPT_NUMBER";
+			break;
+	}
+	return fmt::format("Option is not of type {}", name);
+} (type)) {}
+/* ========== Value getters for all option types ==================================================================== */
+template<> std::string_view
+cAppCmdOption::GetValue<APP_CMD_OPT_STRING>() const {
+	if (auto pValue = std::get_if<std::string>(&m_value))
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_STRING);
+}
+template<> int
+cAppCmdOption::GetValue<APP_CMD_OPT_INTEGER>() const {
+	if (auto pValue = std::get_if<int>(&m_value))
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_INTEGER);
+}
+template<> bool
+cAppCmdOption::GetValue<APP_CMD_OPT_BOOLEAN>() const {
+	if (auto pValue = std::get_if<bool>(&m_value))
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_BOOLEAN);
+}
+template<> double
+cAppCmdOption::GetValue<APP_CMD_OPT_NUMBER>() const {
+	if (auto pValue = std::get_if<double>(&m_value))
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_NUMBER);
+}
+template<> const cSnowflake&
+cAppCmdOption::GetValue<APP_CMD_OPT_CHANNEL>() const {
+	if (auto pValue = std::get_if<cSnowflake>(&m_value); pValue && m_type == APP_CMD_OPT_CHANNEL)
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_CHANNEL);
+}
+template<> const cSnowflake&
+cAppCmdOption::GetValue<APP_CMD_OPT_ROLE>() const {
+	if (auto pValue = std::get_if<cSnowflake>(&m_value); pValue && m_type == APP_CMD_OPT_ROLE)
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_ROLE);
+}
+template<> const cSnowflake&
+cAppCmdOption::GetValue<APP_CMD_OPT_MENTIONABLE>() const {
+	if (auto pValue = std::get_if<cSnowflake>(&m_value); pValue && m_type == APP_CMD_OPT_MENTIONABLE)
+		return *pValue;
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_MENTIONABLE);
+}
+template<> std::pair<chUser, chPartialMember>
+cAppCmdOption::GetValue<APP_CMD_OPT_USER>() const {
+	if (auto pValue = std::get_if<user_data>(&m_value))
+		return { &pValue->user, pValue->member ? &*pValue->member : nullptr };
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_USER);
+}
+/* ========== Helper value mover ==================================================================================== */
+std::string
+cAppCmdOption::move_value_string() {
+	if (auto pValue = std::get_if<std::string>(&m_value))
+		return std::move(*pValue);
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_STRING);
+}
+std::pair<cUser, std::optional<cPartialMember>>
+cAppCmdOption::move_value_user() {
+	if (auto pValue = std::get_if<user_data>(&m_value))
+		return { std::move(pValue->user), std::move(pValue->member) };
+	throw xAppCmdOptionTypeError(APP_CMD_OPT_USER);
+}
+/* ================================================================================================================== */
+cInteraction::guild_data::guild_data(const json::value& s, const json::value& v): guild_id(s.as_string()), member(v) {}
+/* ================================================================================================================== */
 cInteraction::cInteraction(eInteractionType type, const json::object& o):
 	m_type(type),
-	m_id(json::value_to<std::string_view>(o.at("id"))),
-	m_application_id(json::value_to<std::string_view>(o.at("application_id"))),
+	m_id(o.at("id").as_string()),
+	m_application_id(o.at("application_id").as_string()),
 	m_token(json::value_to<std::string>(o.at("token"))),
 	m_app_permissions(PERM_NONE),
 	m_user([](const json::object& o) -> const json::value& {
@@ -101,7 +195,7 @@ cInteraction::cInteraction(eInteractionType type, const json::object& o):
 	}(o)) {
 	const json::value* p;
 	if ((p = o.if_contains("channel_id")))
-		m_channel_id.emplace(json::value_to<std::string_view>(*p));
+		m_channel_id.emplace(p->as_string());
 	/* If the interaction was triggered from DMs... */
 	if (o.contains("user"))
 		return;
--- Discord/Interaction/InteractionBase.h
+++ Discord/Interaction/InteractionBase.h
@@ -37,10 +37,9 @@ enum eAppCmdOptionType {
 };
 eAppCmdOptionType tag_invoke(boost::json::value_to_tag<eAppCmdOptionType>, const boost::json::value&);
 /* ================================================================================================================== */
-class xInvalidAttributeError : public std::invalid_argument {
+class xAppCmdOptionTypeError : public std::invalid_argument {
 public:
-	xInvalidAttributeError(const char* m)        : std::invalid_argument(m) {}
-	xInvalidAttributeError(const std::string& m) : std::invalid_argument(m) {}
+	xAppCmdOptionTypeError(eAppCmdOptionType);
 };
 /* ================================================================================================================== */
 class cAppCmdOption final {
@@ -48,65 +47,41 @@ private:
 	std::string       m_name;
 	eAppCmdOptionType m_type;
 
-	typedef std::vector<cAppCmdOption> options_t;
-
 	struct user_data {
 		cUser user;
 		std::optional<cPartialMember> member;
-
 		user_data(const json::value&, const json::value*);
 	};
-	std::variant<int, double, bool, std::string, cSnowflake, options_t, user_data> m_value;
+	std::variant<int, double, bool, std::string, cSnowflake, std::vector<cAppCmdOption>, user_data> m_value;
+	/* Helper functions for moving specific values */
+	std::string move_value_string();
+	std::pair<cUser, std::optional<cPartialMember>> move_value_user();
 	
 public:
 	explicit cAppCmdOption(const json::value& v, cPtr<const json::value> r);
 	explicit cAppCmdOption(eAppCmdType, std::string_view, const json::object&);
-
+	/* Simple member getters */
 	eAppCmdOptionType GetType() const noexcept { return m_type;            }
 	std::string_view  GetName() const noexcept { return m_name;            }
 	std::string      MoveName()       noexcept { return std::move(m_name); }
-
+	/* Option getters for subcommands */
 	std::span<const cAppCmdOption> GetOptions() const noexcept;
 	std::span<cAppCmdOption>       GetOptions()       noexcept;
 	std::vector<cAppCmdOption>    MoveOptions()       noexcept;
-
+	/* Const value getters for every supported option type */
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_STRING)
-	std::string_view GetValue() const {
-		if (auto pValue = std::get_if<std::string>(&m_value))
-			return *pValue;
-		throw xInvalidAttributeError("exception");
-	}
+	std::string_view GetValue() const;
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_INTEGER)
-	int GetValue() const {
-		if (auto pValue = std::get_if<int>(&m_value))
-			return *pValue;
-		throw xInvalidAttributeError("exception");
-	}
+	int GetValue() const;
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_BOOLEAN)
-	bool GetValue() const {
-		if (auto pValue = std::get_if<bool>(&m_value))
-			return *pValue;
-		throw xInvalidAttributeError("exception");
-	}
+	bool GetValue() const;
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_NUMBER)
-	double GetValue() const {
-		if (auto pValue = std::get_if<double>(&m_value))
-			return *pValue;
-		throw xInvalidAttributeError("exception");
-	}
+	double GetValue() const;
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_CHANNEL || Type == APP_CMD_OPT_ROLE || Type == APP_CMD_OPT_MENTIONABLE)
-	const cSnowflake& GetValue() const {
-		if (auto pValue = std::get_if<cSnowflake>(&m_value); pValue && m_type == Type)
-			return *pValue;
-		throw xInvalidAttributeError("exception");
-	}
+	const cSnowflake& GetValue() const;
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_USER)
-	std::pair<chUser, chPartialMember> GetValue() const {
-		if (auto pValue = std::get_if<user_data>(&m_value))
-			return { &pValue->user, pValue->member ? &*pValue->member : nullptr };
-		throw xInvalidAttributeError("exception");
-	}
-
+	std::pair<chUser, chPartialMember> GetValue() const;
+	/* Non const value getters where it makes sense */
 	template<eAppCmdOptionType Type> requires (Type == APP_CMD_OPT_CHANNEL || Type == APP_CMD_OPT_ROLE || Type == APP_CMD_OPT_MENTIONABLE)
 	cSnowflake& GetValue() {
 		return const_cast<cSnowflake&>(const_cast<const cAppCmdOption*>(this)->GetValue<Type>());
@@ -116,25 +91,22 @@ public:
 		auto[u, m] = const_cast<const cAppCmdOption*>(this)->GetValue<Type>();
 		return { const_cast<cUser*>(u.Get()), const_cast<cPartialMember*>(m.Get()) };
 	}
-
+	/* Value mover */
 	template<eAppCmdOptionType Type> requires (Type != APP_CMD_OPT_SUB_COMMAND && Type != APP_CMD_OPT_SUB_COMMAND_GROUP)
 	auto MoveValue() {
-		if constexpr (Type == APP_CMD_OPT_STRING) {
-			if (auto v = std::get_if<std::string>(&m_value))
-				return std::move(*v);
-		} else if constexpr (Type == APP_CMD_OPT_USER) {
-			if (auto p = std::get_if<user_data>(&m_value))
-				return std::pair<cUser, std::optional<cPartialMember>>{ std::move(p->user), std::move(p->member) };
-		} else {
+		if constexpr (Type == APP_CMD_OPT_STRING)
+			return move_value_string();
+		else if constexpr (Type == APP_CMD_OPT_USER)
+			return move_value_user();
+		else
 			return GetValue<Type>();
-		}
-		throw xInvalidAttributeError("exception");
 	}
 };
 typedef   hHandle<cAppCmdOption>   hAppCmdOption;
 typedef  chHandle<cAppCmdOption>  chAppCmdOption;
 typedef  uhHandle<cAppCmdOption>  uhAppCmdOption;
 typedef uchHandle<cAppCmdOption> uchAppCmdOption;
+cAppCmdOption tag_invoke(boost::json::value_to_tag<cAppCmdOption>, const json::value&, cPtr<const json::value>);
 /* ========== Forward declarations of interaction types ============================================================= */
 class cAppCmdInteraction;
 class cMsgCompInteraction;
