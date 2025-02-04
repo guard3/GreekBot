6392d42310d50d0e7ebf009091244fc10228f83b
guard3 <cblsss2011@hotmail.com>
2025-02-04 23:36:12 +0200

    More refactoring

--- GreekBot/GreekBot_Leaderboard.cpp
+++ GreekBot/GreekBot_Leaderboard.cpp
@@ -4,34 +4,19 @@
 #include "Utils.h"
 #include <algorithm>
 #include <ranges>
-/* ================================================================================================================== */
-namespace rng = std::ranges;
 /* ========== Helper macros to catch and report errors ============================================================== */
 #define LEADERBOARD_HANDLER_BEGIN try
 #define LEADERBOARD_HANDLER_END catch (const xDiscordError& e) {        \
     cUtils::PrintErr("An error occurred in {}: {}", __func__, e.what());\
     cUtils::PrintErr("Error details: {}", e.errors());                  \
 }
-/* ========== Level role ids ======================================================================================== */
-enum : std::uint64_t {
-	ROLE_ID_MEMBER               = 350264923806367754, // @Member
-	ROLE_ID_ACTIVE_MEMBER        = 352008860904456192, // @Active Member
-	ROLE_ID_ACTIVE_USER          = 352009155864821760, // @Active User
-	ROLE_ID_REALLY_ACTIVE_MEMBER = 353354156783960075, // @Really Active Member
-	ROLE_ID_USUAL_SUSPECT        = 410753563543732225, // @Usual Suspect
-	ROLE_ID_GREEK_LOVER          = 410754841556549633, // @Greek Lover
-	ROLE_ID_PROESTOS             = 414523335595130905, // @Προεστός
-	ROLE_ID_EPITIMOS             = 466238555304230913, // @Επίτιμος
-	ROLE_ID_ELLINOPOULO          = 608687509291008000, // @Ελληνόπουλο
-	ROLE_ID_PALIOS               = 631559446782410752  // @Παλιός
-};
 /* ========== Helper function to calculate a member's level info based on their XP count ============================ */
 struct level_info {
-	std::uint64_t level;         // The current level
-	std::uint64_t level_xp;      // The minimum XP required to reach current level
-	std::uint64_t next_level_xp; // The XP required to reach the next level
+	std::int64_t level;         // The current level
+	std::int64_t level_xp;      // The minimum XP required to reach current level
+	std::int64_t next_level_xp; // The XP required to reach the next level
 };
-static level_info calculate_level_info(std::uint64_t xp) noexcept {
+static level_info calculate_level_info(std::int64_t xp) noexcept {
 	level_info result{ 0, 0, 100 };
 	while (result.next_level_xp < xp) {
 		result.level++;
@@ -77,19 +62,6 @@ static void make_embed(cEmbed& embed, const cUser& user, cColor c, const leaderb
 
 cTask<>
 cGreekBot::process_leaderboard_new_message(cMessage& msg, cPartialMember& member) LEADERBOARD_HANDLER_BEGIN {
-	static constexpr std::uint64_t rank_role_ids[] {
-		ROLE_ID_MEMBER,
-		ROLE_ID_ACTIVE_MEMBER,
-		ROLE_ID_ACTIVE_USER,
-		ROLE_ID_REALLY_ACTIVE_MEMBER,
-		ROLE_ID_USUAL_SUSPECT,
-		ROLE_ID_GREEK_LOVER,
-		ROLE_ID_PROESTOS,
-		ROLE_ID_EPITIMOS,
-		ROLE_ID_ELLINOPOULO,
-		ROLE_ID_PALIOS
-	};
-	static_assert(rng::is_sorted(rank_role_ids), "Must be sorted for binary search");
 	/* Update leaderboard and retrieve the author's total xp */
 	auto db = co_await BorrowDatabase();
 	auto xp = cLeaderboardDAO(db).Update(msg);
@@ -100,36 +72,25 @@ cGreekBot::process_leaderboard_new_message(cMessage& msg, cPartialMember& member
 	/* Calculate member level from xp */
 	auto[level, level_xp, next_level_xp] = calculate_level_info(xp);
 	/* Get the appropriate rank role for the current level, or 0 for no role */
-	std::uint64_t target_role_id;
-	if (level >= 100) {
-		target_role_id = ROLE_ID_PALIOS;
-	} else if (level >= 70) {
-		target_role_id = ROLE_ID_ELLINOPOULO;
-	} else if (level >= 50) {
-		target_role_id = ROLE_ID_EPITIMOS;
-	} else if (level >= 40) {
-		target_role_id = ROLE_ID_PROESTOS;
-	} else if (level >= 35) {
-		target_role_id = ROLE_ID_GREEK_LOVER;
-	} else if (level >= 25) {
-		target_role_id = ROLE_ID_USUAL_SUSPECT;
-	} else if (level >= 20) {
-		target_role_id = ROLE_ID_REALLY_ACTIVE_MEMBER;
-	} else if (level >= 15) {
-		target_role_id = ROLE_ID_ACTIVE_MEMBER;
-	} else if (level >= 10) {
-		target_role_id = ROLE_ID_MEMBER;
-	} else if (level >= 6) {
-		target_role_id = ROLE_ID_ACTIVE_USER;
-	} else {
-		target_role_id = 0;
-	}
+	const cSnowflake target_role_id = [level] {
+		if (level >= 100) return LMG_ROLE_PALIOS;
+		if (level >=  70) return LMG_ROLE_ELLINOPOULO;
+		if (level >=  50) return LMG_ROLE_EPITIMOS;
+		if (level >=  40) return LMG_ROLE_PROESTOS;
+		if (level >=  35) return LMG_ROLE_GREEK_LOVER;
+		if (level >=  25) return LMG_ROLE_USUAL_SUSPECT;
+		if (level >=  20) return LMG_ROLE_REALLY_ACTIVE_MEMBER;
+		if (level >=  15) return LMG_ROLE_ACTIVE_MEMBER;
+		if (level >=  10) return LMG_ROLE_MEMBER;
+		if (level >=   6) return LMG_ROLE_ACTIVE_USER;
+		return cSnowflake();
+	} ();
 	/* Prepare member options with the member's roles */
 	cMemberOptions options;
 	auto& member_roles = options.EmplaceRoles(member.MoveRoles());
 	/* Push rank roles to the end of the role vector */
-	auto rank_roles = rng::partition(member_roles, [](const cSnowflake& role_id) {
-		return !rng::binary_search(rank_role_ids, role_id);
+	auto rank_roles = std::ranges::partition(member_roles, [](const cSnowflake& role_id) {
+		return !std::ranges::binary_search(LMG_RANK_ROLES, role_id);
 	});
 	/* Check the role_ids to be deleted */
 	switch (rank_roles.size()) {
@@ -137,7 +98,7 @@ cGreekBot::process_leaderboard_new_message(cMessage& msg, cPartialMember& member
 			if (target_role_id == 0)
 				co_return;
 			/* Member should have a rank role, so we give them one */
-			member_roles.emplace_back(target_role_id);
+			member_roles.push_back(target_role_id);
 			break;
 		case 1:
 			if (target_role_id == 0) {
@@ -157,8 +118,7 @@ cGreekBot::process_leaderboard_new_message(cMessage& msg, cPartialMember& member
 			if (target_role_id != 0)
 				*it++ = target_role_id;
 			member_roles.erase(it, member_roles.end());
-			break;
-		}
+		}	break;
 	}
 	co_await ModifyGuildMember(LMG_GUILD_ID, msg.GetAuthor().GetId(), options);
 } LEADERBOARD_HANDLER_END
@@ -237,7 +197,7 @@ cGreekBot::process_top(cAppCmdInteraction& i) HANDLER_BEGIN {
 	/* Create embeds for every leaderboard entry */
 	for (auto& lb_entry: lb_entries) {
 		cEmbed& embed = embeds.emplace_back();
-		if (auto it = rng::find(members, lb_entry.user_id, [](const cMember& m) -> const cSnowflake& { return m.GetUser()->GetId(); }); it != end(members)) {
+		if (auto it = std::ranges::find(members, lb_entry.user_id, [](const cMember& m) -> const cSnowflake& { return m.GetUser()->GetId(); }); it != end(members)) {
 			/* If the user is a member of Learning Greek, make a regular embed */
 			make_embed(embed, *it->GetUser(), get_lmg_member_color(*it), &lb_entry);
 		} else {
--- GreekBot/LearningGreek.h
+++ GreekBot/LearningGreek.h
@@ -7,6 +7,31 @@ inline constexpr cSnowflake LMG_GUILD_ID = 350234668680871946;
 /* ========== Learning Greek channels =============================================================================== */
 inline constexpr cSnowflake LMG_CHANNEL_USER_LOG = 469274019565142017;
 inline constexpr cSnowflake LMG_CHANNEL_STARBOARD = 978993330694266920;
+/* ========== Learning Greek roles ================================================================================== */
+inline constexpr cSnowflake LMG_ROLE_ACTIVE_USER          = 352009155864821760; // @Active User
+inline constexpr cSnowflake LMG_ROLE_MEMBER               = 350264923806367754; // @Member
+inline constexpr cSnowflake LMG_ROLE_ACTIVE_MEMBER        = 352008860904456192; // @Active Member
+inline constexpr cSnowflake LMG_ROLE_REALLY_ACTIVE_MEMBER = 353354156783960075; // @Really Active Member
+inline constexpr cSnowflake LMG_ROLE_USUAL_SUSPECT        = 410753563543732225; // @Usual Suspect
+inline constexpr cSnowflake LMG_ROLE_GREEK_LOVER          = 410754841556549633; // @Greek Lover
+inline constexpr cSnowflake LMG_ROLE_PROESTOS             = 414523335595130905; // @Προεστός
+inline constexpr cSnowflake LMG_ROLE_EPITIMOS             = 466238555304230913; // @Επίτιμος
+inline constexpr cSnowflake LMG_ROLE_ELLINOPOULO          = 608687509291008000; // @Ελληνόπουλο
+inline constexpr cSnowflake LMG_ROLE_PALIOS               = 631559446782410752; // @Παλιός
+/* ========== Learning Greek role groups ============================================================================ */
+inline constexpr cSnowflake LMG_RANK_ROLES[] {
+	LMG_ROLE_MEMBER,
+	LMG_ROLE_ACTIVE_MEMBER,
+	LMG_ROLE_ACTIVE_USER,
+	LMG_ROLE_REALLY_ACTIVE_MEMBER,
+	LMG_ROLE_USUAL_SUSPECT,
+	LMG_ROLE_GREEK_LOVER,
+	LMG_ROLE_PROESTOS,
+	LMG_ROLE_EPITIMOS,
+	LMG_ROLE_ELLINOPOULO,
+	LMG_ROLE_PALIOS
+};
+static_assert(std::ranges::is_sorted(LMG_RANK_ROLES), "Must be sorted for binary search");
 /* ========== Emojis ================================================================================================ */
 inline constexpr cSnowflake LMG_EMOJI_HOLY = 409075809723219969;
 /* ========== Common colors ========================================================================================= */
